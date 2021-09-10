a407299fc65d5d225ab1d19e5544e0a7b51134b4
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-09-10 20:07:35 +0700

    8-9 implement redux to user sign in and header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,4 +1,5 @@
 import "./header.styles.scss";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
@@ -30,4 +31,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
