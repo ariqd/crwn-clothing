cf9c069409c9aae5132a50f6aaa562ea623e580c
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-09-15 21:57:25 +0700

    8-12 add cart dropdown component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({ currentUser }) => {
   return (
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => {
           </Link>
         )}
         <CartIcon />
+        <CartDropdown />
       </div>
     </div>
   );
