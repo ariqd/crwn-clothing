c02153e486133b3975ced6875fe4c3a6ff2c7161
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-09-18 13:36:50 +0700

    8-13 toggle cart dropdown using redux

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -6,7 +6,7 @@ import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -29,14 +29,15 @@ const Header = ({ currentUser }) => {
           </Link>
         )}
         <CartIcon />
-        <CartDropdown />
       </div>
+      {!hidden && <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
